11ae38b5eb28eaf945b403e66b0a388413c830d2
samir4ik86 <samir4ik86@gmail.com>
2018-03-28 17:12:10 +0400

    1

--- NVVM InternetMarket/NVVM InternetMarket/ViewModel/ViewModel.cs
+++ NVVM InternetMarket/NVVM InternetMarket/ViewModel/ViewModel.cs
@@ -23,7 +23,13 @@ namespace NVVM_InternetMarket.ViewModel
         public ViewModel()
         {
             var category = new Category { Name = "All products" };
-            category.SubElements.Add(new Category { Name = "Electronics" });
+
+            //Categories
+            category.SubElements.Add(new Category { Name = "Computer and Office" });
+            category.SubElements.Add(new Category { Name = "Sports" });
+            category.SubElements.Add(new Category { Name = "Consumer electronics"});
+
+            //Computer and Office
             category.SubElements.FirstOrDefault().SubElements.Add(new Category { Name = "Mobile Phones" });
             category.SubElements.FirstOrDefault().SubElements.FirstOrDefault().SubElements.Add(new Product { Name = "Galaxy S8", BrandName = "Samsung"});
             category.SubElements.FirstOrDefault().SubElements.FirstOrDefault().SubElements.Add(new Product { Name = "Galaxy S7", BrandName = "Samsung" });
@@ -31,10 +37,36 @@ namespace NVVM_InternetMarket.ViewModel
             category.SubElements.FirstOrDefault().SubElements.FirstOrDefault().SubElements.Add(new Product { Name = "Galaxy A8 2015", BrandName = "Samsung" });
             category.SubElements.FirstOrDefault().SubElements.FirstOrDefault().SubElements.Add(new Product { Name = "Galaxy Note 9", BrandName = "Samsung" });
 
+            //Sports
+            category.SubElements[1].SubElements.Add(new Category { Name = "Boots" });
+            category.SubElements[1].SubElements.FirstOrDefault().SubElements.Add(new Product { Name = "Shoes", BrandName = "Nike" });
+
+            //Consumer electronics
+            category.SubElements[2].SubElements.Add(new Category { Name = "Home Audio and Video" });
+            category.SubElements[2].SubElements.Add(new Category { Name = "Portable Audio and Video" });
+            category.SubElements[2].SubElements.Add(new Category { Name = "Video Games" });
+            //Television
+            category.SubElements[2].SubElements[0].SubElements.Add(new Category { Name = "Television" });
+            category.SubElements[2].SubElements[0].SubElements[0].SubElements.Add(new Product { Name = "SM9013", BrandName = "Samsung"});
+            category.SubElements[2].SubElements[0].SubElements[0].SubElements.Add(new Product { Name = "G551080", BrandName = "LG" });
+            //Portable Audio and Video
+            category.SubElements[2].SubElements[0].SubElements.Add(new Category { Name = "Console" });
+            category.SubElements[2].SubElements[0].SubElements[1].SubElements.Add(new Product { Name = "Play Station 2", BrandName = "Sony" });
+            category.SubElements[2].SubElements[0].SubElements[1].SubElements.Add(new Product { Name = "Play Station 3", BrandName = "Sony" });
+            category.SubElements[2].SubElements[0].SubElements[1].SubElements.Add(new Product { Name = "Play Station 4", BrandName = "Sony" });
+            category.SubElements[2].SubElements[0].SubElements[1].SubElements.Add(new Product { Name = "X-BOX ONE", BrandName = "Microsoft" });
+
             Categories = new ObservableCollection<Element>();
             Categories.Add(category);
-
         }
+        private Product _currentProduct;
+
+        public Product CurrentProduct
+        {
+            get { return _currentProduct; }
+            set { _currentProduct = value; }
+        }
+
 
         private ObservableCollection<Element> _categories;
 
