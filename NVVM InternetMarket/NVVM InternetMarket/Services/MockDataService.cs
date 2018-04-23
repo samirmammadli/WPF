234185cdedaf280458ca2d443dd95cc5b81652be
samir4ik86 <samir4ik86@gmail.com>
2018-04-23 16:30:40 +0400

    1

--- NVVM InternetMarket/NVVM InternetMarket/Services/MockDataService.cs
+++ NVVM InternetMarket/NVVM InternetMarket/Services/MockDataService.cs
@@ -1,6 +1,7 @@
 ﻿using NVVM_InternetMarket.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
