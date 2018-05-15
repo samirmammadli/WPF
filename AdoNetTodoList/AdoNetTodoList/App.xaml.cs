3d93b1848d49dd61c563984b9eccb67a2b07a181
samir4ik86 <samir4ik86@gmail.com>
2018-05-15 14:56:39 +0400

    1

---
+++ AdoNetTodoList/AdoNetTodoList/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdoNetTodoList
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var mainWindow = new View.AppView();
+            mainWindow.Show();
+        }
+    }
+}
