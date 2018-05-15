9d4ee74dd159e12dca25c03b28bb0e9039af6a5a
samir4ik86 <samir4ik86@gmail.com>
2018-05-15 17:17:39 +0400

    1

---
+++ AdoNetTodoList/AdoNetTodoList/Model/User.cs
@@ -0,0 +1,43 @@
+﻿using AdoNetTodoList.Tools;
+using System;
+
+namespace AdoNetTodoList.Model
+{
+    class User : ObservableObject
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; OnPropertyChanged(); }
+        }
+
+        private DateTime regDate;
+        public DateTime RegDate
+        {
+            get { return regDate; }
+            set { regDate = value; OnPropertyChanged(); }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; OnPropertyChanged(); }
+        }
+    }
+}
